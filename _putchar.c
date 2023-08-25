092fc7245e8fde6d45975f63df4a8b0220361731
PingOfDeathSA <ronaldnt8@gmail.com>
2023-08-25 08:17:09 -0700

    test

---
+++ _putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+/**
+
+_putchar - prints a character to the output
+@character: The character to be displayed
+Return: 1 if successful, -1 on encountering an error
+*/
+int _putchar(char charTor)
+{
+	return (write(1, &charTor, 1));
+}
+
