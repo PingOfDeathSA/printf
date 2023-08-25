30c1e6f6bf700c09cfb96762aa360c741490bdb7
PingOfDeathSA <ronaldnt8@gmail.com>
2023-08-25 02:41:50 -0700

    test

---
+++ handle_get_flags.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * handle_get_flags - Determine active flags
+ * @format: Formatted string for argument printing
+ * @i: Parameter to consider
+ * Return: Active flags
+ */
+
+int handle_get_flags(const char *format, int *i_)
+{
+
+	int F_, initial_i;
+	int flags_value = 0;
+	const char FLAGS_Charec_value[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_argument_value[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (initial_i = *i_ + 1; format[initial_i] != '\0'; initial_i++)
+	{
+		for (F_ = 0; FLAGS_Charec_value[F_] != '\0'; F_++)
+			if (format[initial_i] == FLAGS_Charec_value[F_])
+			{
+				flags_value |= FLAGS_argument_value[F_];
+				break;
+			}
+
+		if (FLAGS_Charec_value[F_] == 0)
+			break;
+	}
+
+	*i_ = initial_i - 1;
+
+	return (flags_value);
+}
+
+
